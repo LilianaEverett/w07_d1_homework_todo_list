febcf8bf419e8c5c02f80ca4ce88b3a6c7121b50
Liliana Everett <everettliliana@gmail.com>
2019-10-20 13:40:03 +0100

    add pic my app

--- src/app.js
+++ src/app.js
@@ -5,26 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
         el: "#app",
         data: {
             toDos: [
-                {name:"Buy shopping", checked: false, highPriority: true},
-                {name:"Clean bathroom", checked: true, highPriority: true},
-                {name:"Car's MOT", checked: false, highPriority: false}
+                {name:"Shopping", checked: false, priority: "Low Priority"},
+                {name:"Clean bathroom", checked: true, priority: "High Priority"},
+                {name:"Study Javascript", checked: false, priority: "High Priority"}
             ],
-            newToDo: ""
+            newToDo: {name:"", checked: false, priority: ""}
+            // how to test the 2 way binding: newToDo:"test"
         },
         methods: {
-            addNewToDo: function () {
-                this.toDos.push({
-                    name: this.newToDo,
-                    checked: false,
-                    highPriority: this.newToDo
-                });
-                this.newToDo = "";
+            addNewToDo: function() {
+                this.toDos.push(this.newToDo);
+                this.newToDo = {name:"", checked: false, priority: ""}
+                // after we add a new to do it needs to reset to an empty this.newToDo = ""
             },
             checkToDo: function (index) {
                 this.toDos[index].checked = true;
-            },
-            checkPriority: function (index) {
-                this.toDos[index].highPriority = true;
             }
         }
     });
